27611a4698ca13d07017107f8ace902d6b47c891
Eva Touboulic <etouboulic@gmail.com>
2025-09-15 12:42:56 +0200

    compress img

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,9 @@ class Item < ApplicationRecord
 
   accepts_nested_attributes_for :items_tags, allow_destroy: true
 
-  has_many_attached :photos
+  has_many_attached :photos do |attachable|
+    attachable.variant :small, resize_to_limit: [1920, 1080], saver: { quality: 80 }
+  end
 
   enum status: { owned: 0, wanted: 1 }
 
