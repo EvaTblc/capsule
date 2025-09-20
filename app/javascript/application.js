8c5273805e8effe3a78cc194abbc7de122f82278
Eva Touboulic <etouboulic@gmail.com>
2025-09-20 10:48:22 +0200

    abort

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 import "bootstrap"
 import "@popperjs/core"
 
 window.__BarcodeStimulusApp__ ||= Application.start()
 window.__BarcodeStimulusApp__.register("barcode", BarcodeController)
-console.log("[app] Stimulus boot forced, barcode controller registered")
+
